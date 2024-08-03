9a20b0401c17c6a9f297321f67899fb33e074e01
“kelciatkinson” <kelciatkinson@gmail.com>
2024-08-02 19:37:12 -0500

    added new html file for task 3 and targets css for status icon,  created 2-hbnb.py to add new api route

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    const chosenAmenities = {};
+    $('input[type="checkbox"]').change(function() {
+      const amenityID = $(this).data("id")
+      const amenityName = $(this).data("name")
+  
+      if ($(this).is(':checked')) {
+        chosenAmenities[amenityId] = amenityName;
+      } else {
+        delete chosenAmenities[amentiyID];
+      }
+  
+      let amenitiesList = Object.values(chosenAmenities).join(', ')
+      $('div.amenities h4').text(amenitiesList);
+    });
+
+    $.get("http://0.0.0.0:5003/api/v1/status/", (resp) => {
+        if (resp.status === "OK") {
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
+        }
+    })
+  });
